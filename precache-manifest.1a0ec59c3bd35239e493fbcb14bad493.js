2f9096bfd804e29d8f43249b8f3760412fc1687b
Meghedi Hacopi <hacopian.m@gmail.com>
2020-05-04 15:29:06 -0700

    Updates

---
+++ precache-manifest.1a0ec59c3bd35239e493fbcb14bad493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb59c1f94be7faf4938f410d673d05f3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f94bbe063dcdd4d726ca",
+    "url": "/clicky-game/static/css/main.610b9af8.chunk.css"
+  },
+  {
+    "revision": "01541b388b25a05ce450",
+    "url": "/clicky-game/static/js/2.a5afd6a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.a5afd6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94bbe063dcdd4d726ca",
+    "url": "/clicky-game/static/js/main.3bde292b.chunk.js"
+  },
+  {
+    "revision": "b631d7080dcb249ab8e5",
+    "url": "/clicky-game/static/js/runtime-main.ea51dac7.js"
+  }
+]);
